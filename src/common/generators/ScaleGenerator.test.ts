35452a12eacf9b56e8753182d2aedaa0af688a96
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-07 05:53:58 +0530

    Update Scale Typings and Presets. Add test scenarios

--- src/common/generators/ScaleGenerator.test.ts
+++ src/common/generators/ScaleGenerator.test.ts
@@ -1,9 +1,22 @@
-import {expect, test} from "vitest"
+import { expect, test } from "vitest";
 
-function sum(a,b) {
-    return a+b;
-}
+test("generateScale 2px-grid generates scale from 0..20", () => {});
 
-test("add 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
-});
+test("generateScale 4px-grid generates scale from 0..40", () => {});
+
+//TODO: Recheck range for test name
+test("generateScale 4px-ease generates scale in 0,2,4,..,16,20,24,..40, 48, 56,..112", () => {});
+
+test("generateScale 8px generates scale in 0..80", () => {});
+//TODO: Recheck range for test name
+test("generateScale 8px-ease generates scale in 0,4,8,..,32,40,48,..80, 96, 112,..118", () => {});
+
+test("generateScale geometric-base2 generates scales in the form 0, 2, 4, 8, 16...", () => {});
+
+test("generatesScale fifties generates scales in the range 0, 50, 100, 150, .., 950", () => {});
+
+test("generatesScale hundreds generates scales in the range 0, 50, 100, 200, .., 950", () => {});
+
+test("generateScale with start end values, takes priority over steps", () => {});
+
+test("generateScale with custom config generates correct values", () => {});
--- src/common/generators/ScaleGenerator.ts
+++ src/common/generators/ScaleGenerator.ts
@@ -1,37 +1,112 @@
-// enum Scale {
-//     Twos,
-//     Fours,
-//     Eights,
-//     Hundreds,
-//     TShirt,
-//     Fiftys,
-// }
-interface NumericScale {
-    name: string;
-    upperLimit: number;
-    lowerLimit: number;
-}
-
-interface NonNumericScale {
-    values: string[];
-}
-
-export type Scales =
-    | ({ kind: "numeric" } & NumericScale)
-    | ({ kind: "non-numeric" } & NonNumericScale);
-
+//TODO: Add support for ratios for type like for 1.25, 1.333, 1.618 etc.
 export enum Interpolator {
     Linear,
     Geometric,
 }
 
-type Cutoff<T> = { T: Interpolator };
-// const Interpolator = ["Linear", "Geometric"] as const
+// Configurations Possible: Base Interpololator steps and minlimit
+// Previous with cutoffConfig
+// Specifying start and end values or either without specifying steps
+export type ScaleConfig = {
+    base: number;
+    interpolator: Interpolator;
+    steps?: number;
+    minLimit?: number;
+    cutoffConfig?: {
+        cutoff: number;
+        base: number;
+        cutoffType: "pre" | "post";
+        interpolator?: Interpolator;
+    }[];
+    startValues?: number[];
+    endValues?: number[];
+};
 
-export function generateScale<T extends NumericScale | NonNumericScale>(
-    scale: T,
-    interpolator: Interpolator = Interpolator.Linear,
-    interpolatorCutoffs: Cutoff<T>[] | undefined = undefined,
-): Array<String> {
+export const ScalePresets: Record<string, ScaleConfig> = {
+    "2px-grid": {
+        base: 2,
+        interpolator: Interpolator.Linear,
+        steps: 11,
+        minLimit: 0,
+    },
+    "4px-grid": {
+        base: 4,
+        interpolator: Interpolator.Linear,
+        steps: 11,
+        minLimit: 0,
+    },
+    "4px-ease": {
+        base: 4,
+        interpolator: Interpolator.Linear,
+        steps: 21,
+        minLimit: 0,
+        cutoffConfig: [
+            {
+                cutoff: 16,
+                base: 2,
+                cutoffType: "pre",
+                interpolator: Interpolator.Linear,
+            },
+            {
+                cutoff: 40,
+                base: 8,
+                cutoffType: "post",
+                interpolator: Interpolator.Linear,
+            },
+        ],
+    },
+    "8px-grid": {
+        base: 8,
+        interpolator: Interpolator.Linear,
+        steps: 11,
+        minLimit: 0,
+    },
+    "8px-ease": {
+        base: 8,
+        interpolator: Interpolator.Linear,
+        steps: 21,
+        minLimit: 0,
+        cutoffConfig: [
+            {
+                cutoff: 32,
+                base: 4,
+                cutoffType: "pre",
+                interpolator: Interpolator.Linear,
+            },
+            {
+                cutoff: 80,
+                base: 16,
+                cutoffType: "post",
+                interpolator: Interpolator.Linear,
+            },
+        ],
+    },
+    "geometric-base2": {
+        base: 2,
+        interpolator: Interpolator.Geometric,
+        steps: 10,
+        minLimit: 0,
+    },
+    fifties: {
+        base: 50,
+        interpolator: Interpolator.Linear,
+        steps: 21,
+        minLimit: 0,
+    },
+    hundreds: {
+        base: 100,
+        interpolator: Interpolator.Linear,
+        startValues: [0, 50],
+        endValues: [950],
+    },
+};
+
+/**
+ * Generates a scale given the right configuration.
+ * @param config Configuration on how to generate scales.
+ *               Use `ScalePresets` if you don't want to make your own cuomst preset.
+ * @returns an array of scales.
+ */
+export function generateScale(config: ScaleConfig): Array<number> {
     return [];
 }
