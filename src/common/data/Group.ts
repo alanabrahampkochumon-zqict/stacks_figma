1b6b7c9712c356ce7c13da7386a6b27fd64ed558
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-14 05:34:33 +0530

    Add Token Node

--- src/common/data/Group.ts
+++ src/common/data/Group.ts
@@ -1,14 +1,3 @@
-// export class Group {
-//     name: string;
-//     expanded: boolean;
-
-//     constructor(name: string, expanded: boolean = false) {
-//         if (!name.length) throw new Error("Name must not be empty");
-//         this.name = name;
-//         this.expanded = expanded;
-//     }
-// }
-
 export type Group = {
     name: string;
     expanded: boolean;
---
+++ src/common/data/TokenNode.ts
@@ -0,0 +1,33 @@
+import type { Group } from "./Group";
+import type { Token } from "./Token";
+
+/**
+ * Type the encapsulates the atomic unit of Design System.
+ * @note Although you can create Group and Token, it is discouraged
+ *
+ * @export
+ * @typedef {TokenNode}
+ * @property {string} name - Name of the token
+ * @property {string} uid - ID of then token
+ * @property {Group|Token} value - Value of the token. @see Group and @see Token for more details
+ * @property {string} parentId - A reference to the enclosing group. Can be kept undefined if this token is standalone.
+ * @property {string} reference - A reference to another token. Can be kept undefined if this token is the base token.
+ *
+ * @example
+ * {
+ *  name: "red-500",
+ *  uid: "91940fc4-8e7a-43f5-96be-a9f363328590"
+ *  value: { name: "red-500", valueByMode: { default: "#770000" }, type: "color" }
+ *  parentId: "305748a4-6ff4-4f77-b05d-e81397e694ad" // Reference to a primitive group
+ *  reference: undefined // Primitive token
+ * }
+ */
+export type TokenNode = {
+    name: string;
+    uid: string;
+    value: Group | Token;
+    parentId?: string;
+    reference?: string;
+};
+
+// export function createTokenNode
