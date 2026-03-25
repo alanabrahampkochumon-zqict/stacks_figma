d1583addc4a2638cf8601970b3f8daea5eafe43c
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-26 05:06:40 +0530

    test: add test for illegal case of hydrating token

--- tests/common/data/DesignSystem/DesignSystem.HydrateToken.test.ts
+++ tests/common/data/DesignSystem/DesignSystem.HydrateToken.test.ts
@@ -1,10 +1,36 @@
-import { describe, test } from "vitest";
+import { DesignSystem } from "@src/common/data/DesignSystem";
+import { TokenSet } from "@src/common/data/TokenSet";
+import { describe, expect, test } from "vitest";
 import { generateTokenNode } from "../utils/Generators";
 
 describe("Design System: Hydrate Token", () => {
-    const tokens = Array(10)
+    const primitiveTokens = Array(10)
         .fill(0)
-        .map(() => generateTokenNode(undefined, "token", "number"));
+        .map(() =>
+            generateTokenNode(
+                undefined,
+                "token",
+                "number",
+                undefined,
+                undefined,
+                false,
+            ),
+        );
+    const aliasTokens = primitiveTokens.map(token => {
+        
+    })
 
-    test("throws illegal argument error, if primitive token is hydrated", () => {});
+    const primitiveTKS = new TokenSet("primitives", "number", 1, primitiveTokens);
+    const aliasTKS = new TokenSet("alias", "number", 2, )
+
+    test("throws illegal argument error, if primitive token is hydrated", () => {
+        // Given a design system
+        const designSystem = new DesignSystem("ds", [primitiveTKS]);
+
+        // When hydrating a primitive
+        // Then it throws an error
+        expect(() => designSystem.hydrateToken(primitiveTokens[0])).toThrow();
+    });
+
+
 });
