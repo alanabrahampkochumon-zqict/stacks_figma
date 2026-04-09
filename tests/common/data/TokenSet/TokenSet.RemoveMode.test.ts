6265a9f786b7a472ada5037ceedd809edbfdc2d5
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-09 05:54:07 +0530

    test: add mode remove test scenarios for TokenSet

---
+++ tests/common/data/TokenSet/TokenSet.RemoveMode.test.ts
@@ -0,0 +1,9 @@
+import { describe, test } from "vitest";
+
+describe("TokenSet: Remove Mode", () => {
+    test("removes mode, when a valid mode is passed-in", () => {});
+
+    test("throws error, when trying to remove the last mode", () => {});
+
+    test("throws error, when a valid mode is passed-in", () => {});
+});
