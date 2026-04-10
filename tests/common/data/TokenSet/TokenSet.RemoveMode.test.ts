c8f1de7717ab20ec5f547369c8adf7b9d3d08c12
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-04-10 05:54:51 +0530

    test: add test for remove mode

--- tests/common/data/TokenSet/TokenSet.RemoveMode.test.ts
+++ tests/common/data/TokenSet/TokenSet.RemoveMode.test.ts
@@ -1,9 +1,86 @@
-import { describe, test } from "vitest";
+import type { TokenNode } from "@src/common/data/TokenNode";
+import { TokenSet } from "@src/common/data/TokenSet";
+import { describe, expect, test } from "vitest";
+import { generateTokenNode } from "../utils/Generators";
 
 describe("TokenSet: Remove Mode", () => {
-    test("removes mode, when a valid mode is passed-in", () => {});
+    test("removes mode, when a valid mode is passed-in", () => {
+        // Given a tokenset with more than 1 mode
+        const name = "TokenSet";
+        const type = "number";
+        const level = 1;
+        const modes = ["default", "small", "large"];
+        const tokens: TokenNode[] = [
+            generateTokenNode(
+                undefined,
+                "token",
+                type,
+                undefined,
+                undefined,
+                undefined,
+                modes,
+            ),
+        ];
+        const tokenSet = new TokenSet(name, type, level, tokens);
 
-    test("throws error, when trying to remove the last mode", () => {});
+        // When a mode is removed
+        tokenSet.removeMode(modes[2]);
 
-    test("throws error, when a valid mode is passed-in", () => {});
+        // Then, the mode has been removed from existing token0
+        console.log(tokenSet);
+        if (tokenSet.tokens[0].value?.entityType !== "token")
+            return expect.fail();
+        const value = tokenSet.tokens[0].value;
+        expect(Object.keys(value.valueByMode)).toContain(modes[0]);
+        expect(Object.keys(value.valueByMode)).toContain(modes[1]);
+        expect(Object.keys(value.valueByMode)).not.toContain(modes[2]);
+    });
+
+    test("throws error, when trying to remove the last mode", () => {
+        // Given a tokenset with more than 1 mode
+        const name = "TokenSet";
+        const type = "number";
+        const level = 1;
+        const modes = ["default"];
+        const tokens: TokenNode[] = [
+            generateTokenNode(
+                undefined,
+                "token",
+                type,
+                undefined,
+                undefined,
+                undefined,
+                modes,
+            ),
+        ];
+        const tokenSet = new TokenSet(name, type, level, tokens);
+
+        // When a mode is removed
+        // Then, it throws an error
+        expect(() => tokenSet.removeMode(modes[0])).toThrow();
+    });
+
+    test("throws error, when a invalid mode is passed-in", () => {
+        // Given a tokenset with more than 1 mode
+        const name = "TokenSet";
+        const type = "number";
+        const level = 1;
+        const modes = ["default"];
+        const tokens: TokenNode[] = [
+            generateTokenNode(
+                undefined,
+                "token",
+                type,
+                undefined,
+                undefined,
+                undefined,
+                modes,
+            ),
+        ];
+        const tokenSet = new TokenSet(name, type, level, tokens);
+
+        // When a mode is removed
+        // Then, it throws an error
+        expect(() => tokenSet.removeMode("non-existant-mode")).toThrow();
+    });
 });
