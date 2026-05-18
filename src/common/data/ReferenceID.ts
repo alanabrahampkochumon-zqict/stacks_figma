ad2a3e4a15772857c0a54a58052b464b82042f9b
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-18 10:33:19 +0530

    refactor: remove type-based referenceID and its methods

--- src/common/data/ReferenceID.ts
+++ src/common/data/ReferenceID.ts
@@ -83,28 +83,3 @@ export class ReferenceID {
         return this.#uid === other.#uid
     }
 }
-
-// export type ReferenceID = string & { __brand: "__referenceID" };
-
-export const ReferenceIDPrefix = "reference_";
-
-/**
- * Perform a runtime check to ensure that a given string is a {@link ReferenceID}.
- *
- * @param id The ID to check.
- *
- * @returns True if the id is {@link ReferenceID} else False.
- */
-export function isReferenceID(id: any): id is ReferenceID {
-    return typeof id === "string" && id.startsWith(ReferenceIDPrefix);
-}
-
-/**
- * Generate a {@link ReferenceID} using UUID v4.
- * @remarks Use this method to generate reference ID instead of UUID's v4 as that may break the Design System.
- *
- * @returns A {@link ReferenceID} formed by combining {@link ReferenceIDPrefix} and a generated UUID.
- */
-export function generateReferenceID(): ReferenceID {
-    return (ReferenceIDPrefix + v4()) as ReferenceID;
-}
