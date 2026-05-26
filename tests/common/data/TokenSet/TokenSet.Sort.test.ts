e8eb930dd0a1eb0862dceb01911ee5c603220a43
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-05-26 10:24:56 +0530

    test: update tokenset sort  tests to use the new token class

--- tests/common/data/TokenSet/TokenSet.Sort.test.ts
+++ tests/common/data/TokenSet/TokenSet.Sort.test.ts
@@ -1,57 +1,28 @@
-import { createToken } from "@src/common/data/Token";
-import { createTokenNode } from "../../../../src/common/data/TokenNode";
-import { TokenSet } from "@src/common/data/TokenSet";
-import { describe, expect, test } from "vitest";
+import {Token} from "@src/common/data/Token";
+import {TokenSet} from "@src/common/data/TokenSet";
+import {describe, expect, test} from "vitest";
+import {ReferenceID} from "@src/common/data/ReferenceID.ts";
 
-describe("TokenSet Sorting Tests", () => {
+const refIDPool = Array(10).fill(0).map(() => ReferenceID.generate())
+
+describe("TokenSet: Sort", () => {
     test("sort by name, if no comparator is provided", () => {
-        // Given a unsorted tokenset
+        // Given an unsorted TokenSet
         const name = "TokenSet";
         const tokenType = "number";
         const level = 1;
         const tokens = [
-            createTokenNode(
-                "size-100",
-                createToken(tokenType, { default: 10 }),
-                "1",
-            ),
-            createTokenNode(
-                "size-150",
-                createToken(tokenType, { default: 15 }),
-                "2",
-            ),
-            createTokenNode(
-                "size-0",
-                createToken(tokenType, { default: 0 }),
-                "3",
-            ),
-            createTokenNode(
-                "size-50",
-                createToken(tokenType, { default: 5 }),
-                "4",
-            ),
+            new Token(tokenType, "size-100", {default: 10}, [], refIDPool[1]),
+            new Token(tokenType, "size-150", {default: 15}, [], refIDPool[2]),
+            new Token(tokenType, "size-0", {default: 0}, [], refIDPool[3]),
+            new Token(tokenType, "size-50", {default: 5}, [], refIDPool[4]),
         ];
+
         const sortedTokens = [
-            createTokenNode(
-                "size-0",
-                createToken(tokenType, { default: 0 }),
-                "3",
-            ),
-            createTokenNode(
-                "size-50",
-                createToken(tokenType, { default: 5 }),
-                "4",
-            ),
-            createTokenNode(
-                "size-100",
-                createToken(tokenType, { default: 10 }),
-                "1",
-            ),
-            createTokenNode(
-                "size-150",
-                createToken(tokenType, { default: 15 }),
-                "2",
-            ),
+            new Token(tokenType, "size-0", {default: 0}, [], refIDPool[3]),
+            new Token(tokenType, "size-50", {default: 5}, [], refIDPool[4]),
+            new Token(tokenType, "size-100", {default: 10}, [], refIDPool[1]),
+            new Token(tokenType, "size-150", {default: 15}, [], refIDPool[2]),
         ];
         const tokenSet = new TokenSet(name, tokenType, level, tokens);
 
@@ -63,66 +34,28 @@ describe("TokenSet Sorting Tests", () => {
     });
 
     test("sort by comparator, when comparator is provided", () => {
-        // Given a unsorted tokenset
+        // Given an unsorted TokenSet
         const name = "TokenSet";
         const tokenType = "number";
         const level = 1;
         const tokens = [
-            createTokenNode(
-                "size-100",
-                createToken(tokenType, { default: 55 }),
-                "1",
-            ),
-            createTokenNode(
-                "size-150",
-                createToken(tokenType, { default: 35 }),
-                "2",
-            ),
-            createTokenNode(
-                "size-0",
-                createToken(tokenType, { default: 100 }),
-                "3",
-            ),
-            createTokenNode(
-                "size-50",
-                createToken(tokenType, { default: 50 }),
-                "4",
-            ),
+            new Token(tokenType, "size-100", {default: 55}, [], refIDPool[1]),
+            new Token(tokenType, "size-150", {default: 35}, [], refIDPool[2]),
+            new Token(tokenType, "size-0", {default: 100}, [], refIDPool[3]),
+            new Token(tokenType, "size-50", {default: 50}, [], refIDPool[4]),
         ];
         const sortedTokens = [
-            createTokenNode(
-                "size-150",
-                createToken(tokenType, { default: 35 }),
-                "2",
-            ),
-            createTokenNode(
-                "size-50",
-                createToken(tokenType, { default: 50 }),
-                "4",
-            ),
-            createTokenNode(
-                "size-100",
-                createToken(tokenType, { default: 55 }),
-                "1",
-            ),
-            createTokenNode(
-                "size-0",
-                createToken(tokenType, { default: 100 }),
-                "3",
-            ),
+            new Token(tokenType, "size-150", {default: 35}, [], refIDPool[2]),
+            new Token(tokenType, "size-50", {default: 50}, [], refIDPool[4]),
+            new Token(tokenType, "size-100", {default: 55}, [], refIDPool[1]),
+            new Token(tokenType, "size-0", {default: 100}, [], refIDPool[3]),
         ];
         const tokenSet = new TokenSet(name, tokenType, level, tokens);
 
         // When sort function is called on it with a comparator
         tokenSet.sort(
             (a, b) =>
-                Object.values(a.value.entityType == "token" && a)
-                Object.values(
-                    a.value?.entityType === "token" && a.value.valueByMode,
-                )[0] -
-                Object.values(
-                    b.value?.entityType === "token" && b.value.valueByMode,
-                )[0],
+                Object.values(a.valueByMode)[0] - Object.values(b.valueByMode)[0]
         );
 
         // Then, the tokens are sorted by the comparator(here, valueByName)
